6832fc4193e6dc543b0d500c771229f01a5dcc82
José Vitor <josevitorpignataro@gmail.com>
2020-08-24 22:58:21 -0300

    Exercicio-aula-28-08-versao-final

--- main.c
+++ main.c
@@ -8,21 +8,24 @@
 /*Sexto Semestre*/
 
 
+//1) Declaro o struc que representa um no arvore
 struct arvore {
  char pf;
  struct arvore* esquerda;
  struct arvore* direita;
 };
-
+//2) declaro um typedef
 typedef struct arvore Arvore;
 
 int main(int argc, char *argv[]) {
 	
+	//3) inicializa o nó com valor null
 	Arvore* inicializa(void)
 	{
  		return NULL;
 	}
 	
+	//Cria um novo nó
 		Arvore* cria(char ch, Arvore* esq, Arvore* dir){
  		Arvore* n=(Arvore*)malloc(sizeof(Arvore));
  		n->pf = ch;
